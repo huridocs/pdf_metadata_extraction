7e38a18da34965652928007b1c82e0d0d2476c34
Gabo <gabriel.piles.glez@gmail.com>
2024-04-16 14:49:37 +0200

    Run formatter

--- src/send_logs.py
+++ src/send_logs.py
@@ -8,16 +8,16 @@ from data.LogsMessage import Severity, LogsMessage
 def delete_old_messages():
     message = logs_queue.receiveMessage().exceptions(False).execute()
 
-    while message and message['ts'] < time() - 2 * 60 * 60 * 24:
-        logs_queue.deleteMessage(id=message['id']).execute()
+    while message and message["ts"] < time() - 2 * 60 * 60 * 24:
+        logs_queue.deleteMessage(id=message["id"]).execute()
         message = logs_queue.receiveMessage().exceptions(False).execute()
 
 
 def send_logs(extraction_identifier: ExtractionIdentifier, message: str, severity: Severity = Severity.info):
     if severity == Severity.error:
-        config_logger.error(message + ' for ' + extraction_identifier.model_dump_json())
+        config_logger.error(message + " for " + extraction_identifier.model_dump_json())
     else:
-        config_logger.info(message + ' for ' + extraction_identifier.model_dump_json())
+        config_logger.info(message + " for " + extraction_identifier.model_dump_json())
     try:
         delete_old_messages()
         log_message = LogsMessage(
@@ -29,4 +29,3 @@ def send_logs(extraction_identifier: ExtractionIdentifier, message: str, severit
         logs_queue.sendMessage().message(log_message.dump()).execute()
     except Exception as e:
         config_logger.error(f"Error sending logs: {e}")
-
