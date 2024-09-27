eaea29b991f119d2ef223d0ec518abbcf2de4ea0
Ali Altıparmak <ali@huridocs.org>
2024-09-27 14:33:14 +0300

    Handle GPU out of memory

--- src/extractors/pdf_to_multi_option_extractor/multi_labels_methods/SingleLabelSetFitEnglishMethod.py
+++ src/extractors/pdf_to_multi_option_extractor/multi_labels_methods/SingleLabelSetFitEnglishMethod.py
@@ -1,3 +1,4 @@
+import gc
 import os
 import shutil
 from os.path import join, exists
@@ -114,6 +115,10 @@ class SingleLabelSetFitEnglishMethod(MultiLabelMethod):
         trainer.train()
 
         trainer.model.save_pretrained(self.get_model_path())
+
+        del model
+        del trainer
+        gc.collect()
 
     def predict(self, multi_option_data: ExtractionData) -> list[list[Option]]:
         model = SetFitModel.from_pretrained(self.get_model_path(), trust_remote_code=True)
