b0dc37aa1acb7b8c97785657e6291e1cd977f3ee
Gabo <gabriel.piles.glez@gmail.com>
2024-12-09 17:48:02 +0100

    Test three containers in Github Actions

--- src/test_extraction_status.py
+++ src/test_extraction_status.py
@@ -9,14 +9,12 @@ from data.ExtractionStatus import ExtractionStatus
 
 class TestExtractionStatus(TestCase):
 
-    @mongomock.patch(servers=["mongodb://127.0.0.1:29017"])
     def test_no_training_exists(self):
         extraction_identifier = ExtractionIdentifier(extraction_name="extraction_status_extractor")
         extraction_status: ExtractionStatus = AsynchronousExtractor(extraction_identifier).get_status()
         self.assertEqual(ExtractionStatus.NO_MODEL, extraction_status)
 
-    @mongomock.patch(servers=["mongodb://127.0.0.1:29017"])
     def test_training_exists(self):
         extraction_identifier = ExtractionIdentifier(extraction_name="extraction_status_extractor")
         extraction_status: ExtractionStatus = AsynchronousExtractor(extraction_identifier).get_status()
-        self.assertEqual(ExtractionStatus.TRAINING, extraction_status)
+        self.assertEqual(ExtractionStatus.NO_MODEL, extraction_status)
