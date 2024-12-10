4083232ef2576cc954ad4f9e4615def93577922c
Gabo <gabriel.piles.glez@gmail.com>
2024-12-10 11:09:20 +0100

    Add process status

--- src/AsynchronousExtractor.py
+++
@@ -1,9 +0,0 @@
-from data.ExtractionStatus import ExtractionStatus
-
-
-class AsynchronousExtractor:
-    def __init__(self, extraction_identifier):
-        self.extraction_identifier = extraction_identifier
-
-    def get_status(self):
-        return ExtractionStatus.NO_MODEL
--- src/data/ExtractionStatus.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class ExtractionStatus(Enum):
-    NO_MODEL = 0
-    TRAINING = 1
-    TRAINED = 2
-    CLOUD_MODEL = 3
