26a6515f9d21e398878b8fd2ed692bd64671e09b
Gabo <gabriel.piles.glez@gmail.com>
2025-10-01 15:33:06 +0200

    Fix paragraph extractor task name

--- src/domain/TasksNames.py
+++ src/domain/TasksNames.py
@@ -4,4 +4,4 @@ from enum import StrEnum
 class TasksNames(StrEnum):
     CREATE_MODEL_TASK_NAME = "create_model"
     SUGGESTIONS_TASK_NAME = "suggestions"
-    PARAGRAPH_EXTRACTION_TASK_NAME = "paragraph_extraction"
+    PARAGRAPH_EXTRACTION_TASK_NAME = "extract_paragraphs"
